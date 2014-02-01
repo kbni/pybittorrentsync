e121cd4f32e225f9941a1941855ce3d5cade428b
Alex Wilson <alex@kbni.net>
2014-02-01 20:39:12 +1000

    JSON config, added comments

--- btsyncsync.py
+++ btsyncsync.py
@@ -1,3 +1,39 @@
+#!/usr/bin/env python
+"""
+
+Author: Alex Wilson <alex@kbni.net>
+Date: 1st Febuary 2014
+License: WTFPL (http://www.wtfpl.net/)
+Description:
+	Tool to aid in syncing shares amongst BitTorrent Sync hosts.
+	This tool will compare shares on all defined hosts, and any which are
+	in a 'common share' directory (share_dir in the JSON) will be added to
+	all other hosts (under their share_dir). It will not actually make the
+	directory in the OS, and BitTorrent Sync does not do this for you, but
+	this script will dump a mkdir string you can use for that, then re-run
+	the script and the shares will then add.
+
+	BitTorrent Sync API is not required (that's kind of the point of this)
+	I have no wish to sign up for a special API key
+
+In your ~/.btsyncsync.json:
+
+{
+    "host1": {
+        "share_dir": "/storage/btsynced",
+        "host_port": "192.168.2.250:8888",
+        "user_pass": "username:password"
+    },
+    "host2": {
+        "share_dir": "/var/lib/btsynced",
+        "host_port": "192.168.2.253:8888",
+        "user_pass": "username:password"
+    }
+}
+
+share_dir is the directory where common shares will be placed.
+
+"""
 
 import sys
 import re
@@ -23,7 +59,9 @@ if __name__ == "__main__":
 	distshares = {}
 
 	for hostname, hostdetails in hosts.iteritems():
-		_shares, _host, _port, _user, _passwd = hostdetails.split(':', 4)
+		_shares = hostdetails['share_dir']
+		_host, _port = hostdetails['host_port'].split(':', 2)
+		_user, _passwd = hostdetails['user_pass'].split(':', 2)
 		sync_obj = bts.BitTorrentSync(user=_user, passwd=_passwd, host=_host, port=_port)
 		sync_obj._used_fw_keys = []
 		sync_obj._used_ro_keys = []
